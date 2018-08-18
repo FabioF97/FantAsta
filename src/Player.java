34e145797a5dcacd666556f9e5e9383b31381173
Fabio <fabio.polito24@gmail.com>
2018-08-18 16:59:00 +0200

    player

---
+++ src/Player.java
@@ -0,0 +1,19 @@
+/**
+ * this class 
+ * @author Fabio Polito, Fabio Fontana
+ *
+ */
+
+
+public class Player {
+	
+	private String name;
+	private String surname;
+	private String position;
+	private String team;
+	private int value;
+	
+	
+	
+
+}
